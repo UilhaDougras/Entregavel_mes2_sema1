9fe94497c4360a50fa2807b56de1bde8baffc568
UilhaDougras <uilhadougras@gmail.com>
2024-03-07 20:01:54 -0300

    entity, Funcionario e Produto

--- src/main/java/app/entity/Funcionario.java
+++ src/main/java/app/entity/Funcionario.java
@@ -1,5 +1,32 @@
 package app.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Funcionario {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long idFuncionario;
+	
+	@NotBlank(message = "Erro campo vazio")
+	private String Nome;
+	
+	@NotNull(message = "Erro campo vazio")
+	private int Idade;
+	
+	@NotNull(message = "Erro campo vazio")
+	private int Matricula;
 }
--- src/main/java/app/entity/Produto.java
+++ src/main/java/app/entity/Produto.java
@@ -1,5 +1,29 @@
 package app.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Produto {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long idProduto;
+	
+	@NotBlank(message = "Erro campo vazio")
+	private String Nome;
+	
+	@NotNull(message = "Erro campo vazio")
+	private double Valor;
 }
