ad23fd44008074b21826cb407b2df33b58fde453
Saler C. Lourenço <saler.cez@gmail.com>
2024-03-07 20:24:21 -0300

    cria função findAll

--- src/main/java/app/controller/ProdutoController.java
+++ src/main/java/app/controller/ProdutoController.java
@@ -1,5 +1,29 @@
 package app.controller;
 
-public class ProdutoController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import app.entity.Produto;
+import app.service.ProdutoService;
+
+@RestController
+@RequestMapping("/api/produto")
+public class ProdutoController {
+	@Autowired
+	public ProdutoService produtoService;
+	
+	@GetMapping("/findAll")
+	public ResponseEntity<List<Produto>> findAll(){
+		try {
+			return new ResponseEntity<List<Produto>>(this.produtoService.findAll(), HttpStatus.FOUND);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+		}
+	}
 }
--- src/main/java/app/service/ProdutoService.java
+++ src/main/java/app/service/ProdutoService.java
@@ -1,5 +1,20 @@
 package app.service;
 
-public class ProdutoService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import app.entity.Produto;
+import app.repository.ProdutoRepository;
+
+@Service
+public class ProdutoService {
+	@Autowired
+	public ProdutoRepository produtoRepository;
+
+	public List<Produto> findAll() {
+		return this.produtoRepository.findAll();
+	}
+	
 }
