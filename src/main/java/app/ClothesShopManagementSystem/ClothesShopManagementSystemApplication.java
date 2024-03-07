82ada68328e4814acb3fc3aaecd6366252437dbb
Saler C. Lourenço <saler.cez@gmail.com>
2024-03-07 19:10:56 -0300

    commit inicial

---
+++ src/main/java/app/ClothesShopManagementSystem/ClothesShopManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package app.ClothesShopManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothesShopManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothesShopManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/ClothesShopManagementSystem/ClothesShopManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package app.ClothesShopManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothesShopManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
