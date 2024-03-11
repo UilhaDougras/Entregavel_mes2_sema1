ed9edb19f22c4f1791a0f87ddd1db0cdf81b0d19
Saler C. Lourenço <saler.cez@gmail.com>
2024-03-11 19:59:28 -0300

    cria crud para funcionarios

--- src/main/java/app/controller/FuncionarioController.java
+++ src/main/java/app/controller/FuncionarioController.java
@@ -1,5 +1,73 @@
 package app.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import app.entity.Funcionario;
+import app.service.FuncionarioService;
+
+@RestController
+@RequestMapping("/api/funcionario")
 public class FuncionarioController {
+	@Autowired
+	public FuncionarioService funcionarioService;
+
+	@GetMapping("/findAll")
+	public ResponseEntity<List<Funcionario>> findAll() {
+		try {
+			return new ResponseEntity<List<Funcionario>>(this.funcionarioService.findAll(), HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+		}
+	}
+
+	@GetMapping("/findById/{id}")
+	public ResponseEntity<Funcionario> findById(@PathVariable long id) {
+		try {
+			return new ResponseEntity<Funcionario>(this.funcionarioService.findById(id), HttpStatus.FOUND);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+		}
+	}
+
+	@PostMapping("/save")
+	public ResponseEntity<String> save(@RequestBody Funcionario funcionario) {
+		try {
+			this.funcionarioService.save(funcionario);
+			return new ResponseEntity<String>("O funcionario " + funcionario.getNome() + " foi cadastrado com sucesso", HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<String>("Houve o erro: " + e, HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	@PutMapping("/updade/{id}")
+	public ResponseEntity<String> update(@PathVariable long id, @RequestBody Funcionario funcionario){
+		try {
+			this.funcionarioService.update(id, funcionario);
+			return new ResponseEntity<String>("O funcionario " + funcionario.getNome() + " foi atualizado com sucesso", HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<String>("Houve o erro: " + e, HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<String> delete(@PathVariable long id){
+		try {
+			return new ResponseEntity<String>("O funcionario " + this.funcionarioService.delete(id) + " foi deletado com sucesso", HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<String>("Houve o erro: " + e, HttpStatus.BAD_REQUEST);
+		}
+	}
 
 }
--- src/main/java/app/service/FuncionarioService.java
+++ src/main/java/app/service/FuncionarioService.java
@@ -1,5 +1,40 @@
 package app.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import app.entity.Funcionario;
+import app.repository.FuncionarioRepository;
+
+@Service
 public class FuncionarioService {
+	@Autowired
+	public FuncionarioRepository funcionarioRepository;
+
+	public List<Funcionario> findAll() {
+		return this.funcionarioRepository.findAll();
+	}
+
+	public Funcionario findById(long id) {
+		// TODO Auto-generated method stub
+		return this.funcionarioRepository.findById(id).get();
+	}
+
+	public void save(Funcionario funcionario) {
+		this.funcionarioRepository.save(funcionario);
+	}
+
+	public void update(long id, Funcionario funcionario) {
+		funcionario.setIdFuncionario(id);
+		this.funcionarioRepository.save(funcionario);
+	}
+
+	public String delete(long id) {
+		String nome = findById(id).getNome();
+		this.funcionarioRepository.deleteById(id);
+		return nome;
+	}
 
 }
