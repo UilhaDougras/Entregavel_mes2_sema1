7b2da1b9545ac8e56187f3db4f6fe8fb57351a9d
Saler C. Lourenço <saler.cez@gmail.com>
2024-03-12 20:32:38 -0300

    cria filtro em venda

--- src/main/java/app/repository/VendaRepository.java
+++ src/main/java/app/repository/VendaRepository.java
@@ -1,9 +1,15 @@
 package app.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import app.entity.Venda;
 
 public interface VendaRepository extends JpaRepository<Venda, Long>{
-
+	public List<Venda> findByClienteNome(String nome);
+	public List<Venda> findByFuncionarioNome(String nome);
+	@Query("SELECT v FROM Vendas v WHERE v.valor > :valor")
+	public List<Venda> findByValorMaior(double valor);
 }
