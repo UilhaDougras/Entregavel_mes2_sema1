26267565228f48377ae8d46b5e8d70b9e32aa634
Saler C. Lourenço <saler.cez@gmail.com>
2024-03-12 20:29:57 -0300

    cria filtros em Cliente

--- src/main/java/app/repository/ClienteRepository.java
+++ src/main/java/app/repository/ClienteRepository.java
@@ -1,9 +1,15 @@
 package app.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import app.entity.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
-
+	public List<Cliente> findByNome (String nome);
+	public List<Cliente> findByTelefone (String telefone);
+	@Query("SELECT c FROM Cliente c WHERE c.idade > :idade")
+	public List<Cliente> findByIdadeMaior(int idade);
 }
